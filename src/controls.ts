5e16b3d9a55106210150f19c1140972b0ebc07d7
claudsaints <claudiodsantoss7@gmail.com>
2024-08-03 16:06:53 -0300

    diagonal movement

--- src/controls.ts
+++ src/controls.ts
@@ -4,7 +4,30 @@ export const controls = (cursors,mr_al,ani_status) => {
         return
     }
     const speed = 100;
-    if (cursors.left?.isDown){
+    const speedDiag = speed * (1/1.44);
+    //diagonal movement...
+    if (cursors.down?.isDown && cursors.right?.isDown){
+        ani_status = 'mr_idle_down'
+        mr_al.anims.play('mr_run_down',true);
+        mr_al.body.setVelocityX(speedDiag);
+        mr_al.body.setVelocityY(speedDiag);
+    }else if(cursors.down?.isDown && cursors.left?.isDown){
+        ani_status = 'mr_idle_down'
+        mr_al.anims.play('mr_run_down',true);
+        mr_al.body.setVelocityX(-speedDiag);
+        mr_al.body.setVelocityY(speedDiag);
+    }else if(cursors.up?.isDown && cursors.left?.isDown){ 
+        ani_status = 'mr_idle_up'
+        mr_al.anims.play('mr_run_up',true);
+        mr_al.body.setVelocityX(-speedDiag);
+        mr_al.body.setVelocityY(-speedDiag);
+    }else if(cursors.up?.isDown && cursors.right?.isDown){
+        ani_status = 'mr_idle_up'
+        mr_al.anims.play('mr_run_up',true);
+        mr_al.body.setVelocityX(speedDiag);
+        mr_al.body.setVelocityY(-speedDiag);
+        
+    }else if (cursors.left?.isDown){
         ani_status = 'mr_idle_sides'
         mr_al.anims.play('mr_run_sides',true);
         mr_al.setVelocity(-speed,0);
@@ -27,6 +50,5 @@ export const controls = (cursors,mr_al,ani_status) => {
     }else{
         mr_al.play(ani_status);
         mr_al.setVelocity(0,0);
-    }
-  
+    } 
 }
